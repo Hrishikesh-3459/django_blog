fa1163c3e63d0580d14a99415522b58e9b301c77
Hrishikesh-3459 <hrishikeshmm01@gmail.com>
2021-08-31 15:36:08 +0530

    Removed posts array

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 
-posts = [
-    {
-        'author': "Alice",
-        'title': 'Title_1',
-        'content': 'content1',
-        'date_posted': 'date1'
-    },
-    {
-        'author': "Bob",
-        'title': 'Title_2',
-        'content': 'content2',
-        'date_posted': 'date2'
-    }
-]
+
 def home(request):
     context = {
         'posts': Post.objects.all()
